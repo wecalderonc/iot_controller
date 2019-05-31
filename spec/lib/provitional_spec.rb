d447bc442bff91582a24c2bba7d3b01649da5175
Jinara <natica61@gmail.com>
2019-05-31 11:15:09 -0500

    fix bin/shot

--- spec/lib/provitional_spec.rb
+++ spec/lib/provitional_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Provitional do
   describe "#sum" do
     context "two numbers" do
       it "should return the sum of those numbers" do
-        expect(Provitional.sum(2,3)).to eq(0)
+        expect(Provitional.sum(2,3)).to eq(5)
       end
     end
   end
