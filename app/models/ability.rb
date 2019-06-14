01bab15c508e76f3c15c758aa43b871df339328f
wecalderonc <wecalderonc@unal.edu.co>
2019-06-14 09:51:33 -0500

    add basic basic example to ability.rb, tested in local

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,11 @@ class Ability
     user = user.success
     # can :read, :all # permissions for every user, even if not logged in
     if user.present?  # additional permissions for logged in users (they can manage their posts)
-      p "osea, lol"
-      p user
-      if user.admin?  # additional permissions for administrators
-        p "soy admin pero no puedo hacer un c***"
-      end
       if user.support_workers.name = "AcueductoBogota"
-        p user.support_workers.name
-        p "FUCKING SERIOUS"
-        can :manage, Thing
+        can :read, Thing
+        if user.admin?  # additional permissions for administrators
+          can :manage, Thing
+        end
       end
     end
   end
