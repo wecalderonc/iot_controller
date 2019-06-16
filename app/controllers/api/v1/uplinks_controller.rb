61c188ca6318ad362bebc592bcc75f960febe8df
William Calderon <wecalderonc@unal.edu.co>
2019-06-16 17:33:58 -0500

    added get last uplink api

--- app/controllers/api/v1/uplinks_controller.rb
+++ app/controllers/api/v1/uplinks_controller.rb
@@ -3,22 +3,11 @@ module Api
     class UplinksController < ApplicationController
       def index
         last_uplink = Uplink.order(:created_at).last
-        if last_uplink
-          render json: {  id: last_uplink.id,
-                          data: last_uplink.data,
-                          avgsnr: last_uplink.avgsnr,
-                          rssi: last_uplink.rssi,
-                          long: last_uplink.long,
-                          lat: last_uplink.lat,
-                          snr: last_uplink.snr,
-                          station: last_uplink.station,
-                          seqnumber: last_uplink.seqnumber,
-                          time: last_uplink.time,
-                          sec_uplinks: last_uplink.sec_uplinks,
-                          sec_downlinks: last_uplink.sec_downlinks
-                        }, status: :ok
+
+        if last_uplink.present?
+          render json: last_uplink, status: :ok
         else
-          render json: { errors: "last_uplink not found" }, status: :not_found
+          render json: { errors: "There aren't any Uplinks" }, status: :not_found
         end
       end
     end
--- spec/integration/uplinks_controller_spec.rb
+++ spec/integration/uplinks_controller_spec.rb
@@ -5,40 +5,50 @@ RSpec.describe "Uplinks API", :type => :request do
     get 'Retrieves all Uplinks' do
       tags 'Uplinks'
       produces 'application/json'
-      parameter({
-       :in => :header,
-       :type => :string,
-       :name => :Authorization,
-       :required => true
-     })
 
       response '200', 'last uplink found' do
+        let(:user) { create(:user) }
+        let!(:uplink) { create(:uplink) }
+        let(:Authorization) { JsonWebToken.encode({ user_id: user.id }) }
+        parameter({
+         :in => :header,
+         :type => :string,
+         :name => :Authorization,
+         :required => true,
+         :description => 'Client token'
+        })
 
-       let(:Authorization) { 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGY5ZmVmZDUtOTA5Zi00ZWRiLWFiM2YtOGY2YjkwMTdlZWNhIiwiZXhwIjoxNTYwNjQ1ODMyfQ.qNUH2wvW-62GpJXTTDiXHwdB3CgXCS9tSoNz4CE2_yU' }
-        #header 'Authorization', type: :string, value: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGY5ZmVmZDUtOTA5Zi00ZWRiLWFiM2YtOGY2YjkwMTdlZWNhIiwiZXhwIjoxNTYwNjQ1ODMyfQ.qNUH2wvW-62GpJXTTDiXHwdB3CgXCS9tSoNz4CE2_yU'
-        let(:uplink) { create(:uplink) }
         schema type: :object,
-          properties:
-            {
-              id: { type: :string },
-              data: { type: :string },
-              avgsnr: { type: :string },
-              rssi: { type: :string },
-              long: { type: :string },
-              lat: { type: :string },
-              snr: { type: :string },
-              station: { type: :string },
-              seqnumber: { type: :string },
-              time: { type: :string },
-              sec_uplinks: { type: :string },
-              sec_downlinks: { type: :string },
-            },
-          required: [ 'id', 'data', 'avgsnr', 'rssi', 'long', 'lat', 'snr', 'station', 'seqnumber', 'time', 'sec_uplinks', 'sec_downlinks' ]
+          required: [ 'uplink' ],
+          properties: {
+            uplink: {
+              type: :object,
+              required: [ 'id', 'data', 'avgsnr', 'rssi', 'long', 'lat', 'snr', 'station', 'seqnumber', 'time', 'sec_uplinks', 'sec_downlinks' ],
+              items:
+                {
+                  properties: {
+                    id: { type: :string },
+                    data: { type: :string },
+                    avgsnr: { type: :string },
+                    rssi: { type: :string },
+                    long: { type: :string },
+                    lat: { type: :string },
+                    snr: { type: :string },
+                    station: { type: :string },
+                    seqnumber: { type: :string },
+                    time: { type: :string },
+                    sec_uplinks: { type: :string },
+                    sec_downlinks: { type: :string }
+                  }
+              }
+            }
+          }
         run_test!
       end
 
       response '404', 'no uplinks found' do
-        let(:Authorization) { 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZGY5ZmVmZDUtOTA5Zi00ZWRiLWFiM2YtOGY2YjkwMTdlZWNhIiwiZXhwIjoxNTYwNjQ1ODMyfQ.qNUH2wvW-62GpJXTTDiXHwdB3CgXCS9tSoNz4CE2_yU' }
+        let(:user) { create(:user) }
+        let(:'Authorization') { JsonWebToken.encode({ user_id: user.id }) }
         run_test!
       end
     end
