4e7821e19c46012c84ea698d850156059646e042
Daniela PB <danielapatinob98@gmail.com>
2019-06-17 11:23:37 -0500

    adds phone format validator

--- app/models/aqueduct.rb
+++ app/models/aqueduct.rb
@@ -9,4 +9,6 @@ class Aqueduct
   validates_presence_of :phone
 
   has_many :out, :support_workers, type: :SUPPORT_WORKERS, model_class: :User
+
+  VALID_PHONE = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
 end
