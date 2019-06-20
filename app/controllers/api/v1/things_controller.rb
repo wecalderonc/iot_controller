190b72aa46f00aadd2a84222fa727b87c9fa5e0a
wecalderonc <wecalderonc@unal.edu.co>
2019-06-20 15:37:42 -0500

    deleted all the messages uplinks controllers, fixed the things_controlle

--- app/controllers/api/v1/things_controller.rb
+++ app/controllers/api/v1/things_controller.rb
@@ -1,12 +1,6 @@
 module Api
   module V1
     class ThingsController < ApplicationController
-
-      def index
-        response = MientrasTanto.aja(params[:thing_id], params[:message_name].chop)
-        render json: response[:result], status: response[:status]
-      end
-
       def show
         thing = Thing.find(params[:id])
         render json: thing, status: :ok
