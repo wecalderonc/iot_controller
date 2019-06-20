906aeef292d5e2172dcc2db79f6b22d894b6f857
wecalderonc <wecalderonc@unal.edu.co>
2019-06-20 15:41:03 -0500

    change to hash the last_messages

--- app/serializers/thing_serializer.rb
+++ app/serializers/thing_serializer.rb
@@ -19,14 +19,13 @@
 
     #Return json with the last MESSAGES saved in database of the Device(Thing)
     def last_messages
-      customized_last_messages = []
+      customized_last_messages = {}
       custom_message = {}
       #Hash.new(accumulator: {}, alarm: {}, batteryLevel: {}, valvePosition: {}, sensor1: {}, sensor2: {},
        #         sensor3: {}, sensor4: {}, uplinkBDownlink: {}, timeUplink: {})
       MESSAGES.each do |message_name|
-        hola = object.uplinks.send(message_name.to_s).order(:created_at).last
-        custom_message[message_name] = hola
-        customized_last_messages.push(custom_message[message_name])
+        custom_message[message_name] = object.uplinks.send(message_name.to_s).order(:created_at).last
+        customized_last_messages[message_name] = custom_message[message_name]
       end
 
       customized_last_messages
