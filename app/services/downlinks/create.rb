1a391ce333f7c78ae82b41feb87265c39f60870f
Jinara <natica61@gmail.com>
2019-06-27 14:47:53 -0500

    clean unused files on this pr

--- app/services/downlinks/create.rb
+++
@@ -1,2 +0,0 @@
-module Downlinks::Create
-end
--- app/services/downlinks/create/execute.rb
+++
@@ -1,7 +0,0 @@
-module Downlinks::Create
-  _, Execute = Common::TxMasterBuilder.new do
-    step :validation,                     with: Common::Operations::Validator.(:create, :downlink)
-    #map  :build_params,                   with: Addresses::Create::BuildParams.new
-    step :update_shadow_desired_state,    with: Amazon::Iotdata::UpdateShadowDesiredState.new
-  end.Do
-end
