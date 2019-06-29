77f090f86cecb6e54938bdd1b0508db025230461
wecalderonc <wecalderonc@unal.edu.co>
2019-06-28 21:31:58 -0500

    add endpoint for things index

--- spec/integration/things_controller_spec.rb
+++ spec/integration/things_controller_spec.rb
@@ -2,6 +2,40 @@ require 'swagger_helper'
 require 'rails_helper'
 
 RSpec.describe "Things API", :type => :request do
+  path "/api/v1/things" do
+     get 'Retrieves all Things' do
+      tags 'Things'
+      produces 'application/json'
+
+      response '200', 'things founded' do
+        let(:user) { create(:user) }
+        let!(:thing) { create(:thing) }
+        let!(:thing2) { create(:thing) }
+        let!(:thing2) { create(:thing) }
+        let(:Authorization) { JsonWebToken.encode({ user_id: user.id }) }
+        parameter({
+         :in => :header,
+         :type => :string,
+         :name => :Authorization,
+         :required => true,
+         :description => 'Client token'
+        })
+
+        schema type: :object,
+          required: thing_fields_required,
+          properties: thing_properties
+
+        run_test!
+      end
+
+      response '404', 'no things found' do
+        let(:user) { create(:user) }
+        let(:'Authorization') { JsonWebToken.encode({ user_id: user.id }) }
+        run_test!
+      end
+    end
+  end
+
   path "/api/v1/things/{id}" do
     get 'Retrieves a thing' do
       tags 'Things'
