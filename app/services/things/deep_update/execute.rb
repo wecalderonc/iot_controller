bb11ce96c6b52d7cb986c6082d221f183439dcc3
Jinara <natica61@gmail.com>
2019-07-02 15:17:56 -0500

    remove unused code

--- app/services/things/deep_update/execute.rb
+++
@@ -1,6 +0,0 @@
-module Things::UpdateShadow
-  _, Execute = Common::TxMasterBuilder.new do
-    step :validation,       with: Common::Operations::Validator.(:update_state, :shadow)
-    step :update_shadow,    with: Things::UpdateShadow::Update.new
-  end.Do
-end
