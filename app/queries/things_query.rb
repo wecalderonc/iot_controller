f162e85a78d2cd2eaae674ba383babf13fc50677
Daniela PB <danielapatinob98@gmail.com>
2019-07-03 18:32:47 -0500

    adds query tests

--- app/queries/things_query.rb
+++ app/queries/things_query.rb
@@ -12,6 +12,17 @@ class ThingsQuery
     sort_collection(@thing.uplinks.alarm)
   end
 
+  def date_uplinks_filter(date, model)
+    start_date, end_date = date.values_at(:start_date, :end_date)
+
+    sort_collection(
+      @thing.uplinks(:u)
+      .where("u.time >= {start_date} and u.time <= {end_date}")
+      .params(start_date: start_date, end_date: end_date)
+      .send(model)
+    )
+  end
+
   private
 
   def sort_collection(collection)
--- spec/controllers/api/v1/accumulators_report_controller_spec.rb
+++ spec/controllers/api/v1/accumulators_report_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Api::V1::AccumulatorsReportController, :type => :request do
   let(:header) { { 'Authorization' => JsonWebToken.encode({ user_id: user.id }) } }
 
   describe "GET/index generate CSV" do
-    context "results found" do
+    context "results not found" do
       it "generate return a JSON" do
         get '/api/v1/accumulators_report', headers: header
 
@@ -24,6 +24,22 @@ RSpec.describe Api::V1::AccumulatorsReportController, :type => :request do
 
         expect(response.headers["Content-Type"]).to eq("text/csv")
         expect(response.status).to eq(200)
+      end
+    end
+
+    context "date filter in params" do
+      let(:start_date)   { (Time.now - 2.days).to_time.to_i.to_s }
+      let(:end_date)     { Time.now.to_time.to_i.to_s }
+      let(:uplink)       { create(:uplink, time: end_date) }
+
+      let!(:accumulator) { create(:accumulator, uplink: uplink) }
+      let(:params)       { { start_date: start_date, end_date: end_date } }
+
+      it "generate a CSV" do
+        get '/api/v1/accumulators_report', headers: header, params: params
+
+        expect(response.headers["Content-Type"]).to eq("text/csv")
+        expect(response.status).to eq(210)
       end
     end
   end
