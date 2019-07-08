7caaa6257782ab8b49db52f15ac745953f4b73b7
Jinara <natica61@gmail.com>
2019-07-08 17:43:43 -0500

    try sidekiq

--- app/workers/check_accumulator_worker.rb
+++ app/workers/check_accumulator_worker.rb
@@ -1,6 +1,8 @@
 class CheckAccumulatorWorker
   include Sidekiq::Worker
 
+  sidekiq_options queue: 'downlinks', retry: false, backtrace: true
+
   def perform(input)
     Shadows::Update::Execute.(input)
   end
