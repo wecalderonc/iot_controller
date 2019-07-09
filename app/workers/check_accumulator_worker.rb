ccfacde658dec91137be68f2acd41b403e12d566
William Calderon <wecalderonc@unal.edu.co>
2019-07-09 01:56:03 -0500

    symbolize keys and symbolize some values from the input

--- app/workers/check_accumulator_worker.rb
+++ app/workers/check_accumulator_worker.rb
@@ -4,6 +4,8 @@ class CheckAccumulatorWorker
   sidekiq_options queue: 'downlinks', retry: false, backtrace: true
 
   def perform(input)
+    input = input.deep_symbolize_keys
+    input = { value: input[:value], input_method: input[:input_method].to_sym, thing_name: input[:thing_name], action: input[:action].to_sym, type: input[:type].to_sym }
     Shadows::Update::Execute.(input)
   end
 end
