82f5c54487bccf5536f801f82baa2c3770fc2680
wecalderonc <wecalderonc@unal.edu.co>
2019-07-23 10:37:39 -0500

    fix comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,21 +1,9 @@
 class Ability
   include CanCan::Ability
 
-
   def initialize(user)
     user = user.success
-    #send("#{user.role}_abilities", user)
-    citizen = "citizen"
-    #send("#{citizen}_abilities", user)
     can :manage, :all
-  end
-
-  def admin_abilities(user)
-    can :manage, :all
-  end
-
-  def citizen_abilities(user)
-    can :manage, Thing, operator: { id: user.id }
   end
 
 end
