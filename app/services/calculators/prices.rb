1a64ebb7041c1db36e528dc85a2c583f41efc216
Jinara <natica61@gmail.com>
2019-07-24 14:14:35 -0500

    fix calc tests

--- app/services/calculators/prices.rb
+++ app/services/calculators/prices.rb
@@ -1,21 +1,39 @@
 module Calculators::Prices
   GetPrice = -> input do
-    last_acc = input[:thing].last_accumulators.last
-    price = Price.by_unit(input[:unit]).value
+    last_acc = input[:last_acc]
+    price = Price.by_unit(input[:unit])
 
-    pp "precio por #{input[:unit]}: #{price}"
-    pp "conteos = #{last_acc.int_value}"
-    pp "conteos por unidad = #{input[:thing].units[input[:unit]]}"
-    Dry::Monads::Success.new(price: price * (last_acc.int_value / input[:thing].units[input[:unit]]))
+    if price.present?
+      aja = price.value * (last_acc.int_value / input[:thing].units[input[:unit]])
+      input.merge!(price: aja)
+      Dry::Monads::Success.new(input)
+    else
+      Dry::Monads::Failure.new(Errors.general_error("There are any configured prices", self.class))
+    end
+  end
+
+  GetLastAccumulators = -> input do
+    last_acc = input[:thing].last_accumulators.last
+    if last_acc.present?
+      input.merge!(last_acc: last_acc)
+      Dry::Monads::Success.new(input)
+    else
+      Dry::Monads::Failure.new(Errors.general_error("No acc", self.class))
+    end
   end
 
   ConvertToCurrency = -> input do
-    Dry::Monads::Success.new(input[:price])
+    if ['COP', 'USD'].include? input[:currency]
+      Dry::Monads::Success.new(input[:price])
+    else
+      Dry::Monads::Failure.new(Errors.general_error("The currency is not valid", self.class))
+    end
   end
 
   #repensar este nombre
   _, ByThingUnit = Common::TxMasterBuilder.new do
     step :validation,            with: Common::Operations::Validator.(:get_price, :thing)
+    step :get_last_acc,          with: Calculators::Prices::GetLastAccumulators
     step :get_price,             with: Calculators::Prices::GetPrice
     step :convert_to_currency,   with: Calculators::Prices::ConvertToCurrency
   end.Do
--- spec/services/calculators/prices_spec.rb
+++ spec/services/calculators/prices_spec.rb
@@ -28,34 +28,57 @@ RSpec.describe Calculators::Prices do
 
     context "The current currency is the same asked currency" do
       it "should return the price of tha" do
-        price1 = create(:price)
+        price1 = create(:price, unit: 'cubic_metter')
         price2 = create(:price, value: 2000)
-        create(:accumulator, uplink: uplink, value: 200, price: price1)
-        create(:accumulator, uplink: uplink, value: 400, price: price2)
+
+        create(:accumulator, uplink: uplink1, value: "C8")
+        create(:accumulator, uplink: uplink2, value: "190")
 
         currency = 'USD'
 
         result = subject::ByThingUnit.new.(thing: thing, unit: unit, currency: currency)
 
         expect(result).to be_success
-        expect(result.success).to eq(1.3)
+        expect(result.success).to eq(4000)
       end
     end
 
-    context "There are no accumulators" do
+    context "There are no Thing" do
       it "should return a failure result" do
-        result = subject::ByThingUnit.(thing, unit, currency)
+        result = subject::ByThingUnit.new.(unit: unit, currency: currency)
 
         expect(result).to be_failure
-        expect(result.failure[:message]).to eq("There are any accumulators")
+        expect(result.failure[:message]).to eq(:thing => ["is missing"])
+
+        result = subject::ByThingUnit.new.(thing: {}, unit: unit, currency: currency)
+
+        expect(result).to be_failure
+        expect(result.failure[:message]).to eq(:thing => ["must be filled"])
+
+        result = subject::ByThingUnit.new.(thing: User, unit: unit, currency: currency)
+
+        expect(result).to be_failure
+        expect(result.failure[:message]).to eq(:thing => ["must be Thing"])
+      end
+    end
+
+    context "There are not accumulators" do
+      it "should return the price of tha" do
+        price1 = create(:price, unit: 'cubic_metter')
+        price2 = create(:price, value: 2000)
+
+        result = subject::ByThingUnit.new.(thing: thing, unit: unit, currency: currency)
+
+        expect(result).to be_failure
+        expect(result.failure[:message]).to eq("No acc")
       end
     end
 
     context "There are not prices" do
       it "should return a failure result" do
-        create(:accumulator, uplink: uplink, value: 200)
+        create(:accumulator, uplink: uplink2, value: "190")
 
-        result = subject::ByThingUnit.(thing, unit, currency)
+        result = subject::ByThingUnit.new.(thing: thing, unit: unit, currency: currency)
 
         expect(result).to be_failure
         expect(result.failure[:message]).to eq("There are any configured prices")
@@ -64,30 +87,26 @@ RSpec.describe Calculators::Prices do
 
     context "There are not prices" do
       it "should return a failure result" do
-        price1 = create(:price)
-        price2 = create(:price, price: 2000)
-        create(:accumulator, uplink: uplink, value: 200, price: price1)
-        create(:accumulator, uplink: uplink, value: 400, price: price2)
+        create(:price, value: 2000)
+        create(:accumulator, uplink: uplink2, value: "190")
 
-        unit = :aja
+        unit = 'aja'
 
-        result = subject::ByThingUnit.(thing, unit, currency)
+        result = subject::ByThingUnit.new.(thing: thing, unit: unit, currency: currency)
 
         expect(result).to be_failure
-        expect(result.failure[:message]).to eq("There are any configured prices for #{unit}")
+        expect(result.failure[:message]).to eq("There are any configured prices")
       end
     end
 
     context "The currency does not exist" do
       it "should return a failure result" do
-        price1 = create(:price)
-        price2 = create(:price, price: 2000)
-        create(:accumulator, uplink: uplink, value: 200, price: price1)
-        create(:accumulator, uplink: uplink, value: 400, price: price2)
+        create(:price, value: 2000)
+        create(:accumulator, uplink: uplink2, value: "190")
 
-        currency= 'aja'
+        currency = 'aja'
 
-        result = subject::ByThingUnit.(thing, unit, currency)
+        result = subject::ByThingUnit.new.(thing: thing, unit: unit, currency: currency)
 
         expect(result).to be_failure
         expect(result.failure[:message]).to eq("The currency is not valid")
