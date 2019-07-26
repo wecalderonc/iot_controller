24df392be8e80b80081b4f7289c76f2d991d3f14
Daniela PB <danielapatinob98@gmail.com>
2019-07-26 14:36:08 -0500

    move GetUplinkDate, to utils

--- lib/things_utils.rb
+++
@@ -1,7 +0,0 @@
-class ThingsUtils
-
-  GetUplinkDate = -> accumulator do
-    date = accumulator.uplink.created_at
-    date.present? ? date.strftime('%a %d %b %Y') : ""
-  end
-end
--- spec/lib/utils_spec.rb
+++ spec/lib/utils_spec.rb
@@ -33,4 +33,28 @@ RSpec.describe Utils do
       end
     end
   end
+
+  describe "#parse_date" do
+    let(:result) { described_class.parse_date(date) }
+
+    context "uplink created_at is present" do
+      let(:accumulator) { create(:accumulator) }
+      let(:date) { accumulator.uplink.created_at }
+      let(:parsed_date) { date.strftime('%a %d %b %Y') }
+
+      it "should return uplink created_at" do
+        expect(result).to eq(parsed_date)
+      end
+    end
+    context "uplink created_at is present" do
+      let(:accumulator) { create(:accumulator) }
+      let(:date) { accumulator.uplink.created_at }
+
+      it "should return uplink created_at" do
+        accumulator.uplink.update(created_at: nil)
+
+        expect(result).to eq("")
+      end
+    end
+  end
 end
