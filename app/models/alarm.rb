2c3306b7bc6f901745a8cfe914694faa0c606f95
jmoreno-zw <jmoreno@procibernetica.com>
2019-07-25 21:34:15 -0500

    save changes for continue tomorrow

--- app/models/alarm.rb
+++ app/models/alarm.rb
@@ -1,8 +1,13 @@
 class Alarm
   include Neo4j::ActiveNode
   property :value, type: String
+  property :date, type: Date
+  property :viewed, type: Boolean
 
   validates :value, presence: true
+  validates :date, presence: true
+  validates :viewed, presence: true
 
   has_one :out, :uplink, type: :BELONGS_TO
+
 end
--- spec/models/alarm_spec.rb
+++ spec/models/alarm_spec.rb
@@ -3,5 +3,22 @@ require 'rails_helper'
 RSpec.describe Alarm, type: :model do
 
   it { is_expected.to define_property :value, :String }
+  it { is_expected.to define_property :date, :Date}
+  it { is_expected.to define_property :viewed, :Boolean}
   it { is_expected.to have_one(:uplink).with_direction(:out) }
+  it { is_expected.to have_one(:type) }
+
+  describe "Validations" do
+    it "date and value are required" do
+      expect(subject).to_not be_valid
+
+      expected_errors = {
+        :date=>["can't be blank"],
+        :value=>["can't be blank"],
+        :viewed=>["can't be blank"],
+      }
+
+      expect(subject.errors.messages).to eq(expected_errors)
+    end
+  end
 end
