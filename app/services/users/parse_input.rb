715f2f819e85b7bf944327895a94b0bb03f6f86c
wecalderonc <wecalderonc@unal.edu.co>
2019-08-02 16:27:25 -0500

    add integration spec 200

--- app/services/users/parse_input.rb
+++ app/services/users/parse_input.rb
@@ -4,6 +4,7 @@ class Users::ParseInput
   include Dry::Transaction::Operation
 
   def call(input)
+    p input
     input.tap do |field|
       field[:gender] = field[:gender].to_sym
       field[:id_type] = field[:id_type].to_sym
