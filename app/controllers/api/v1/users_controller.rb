c3c83f96f2a871c162cfdc2658a83f4bbac4dd07
wecalderonc <wecalderonc@unal.edu.co>
2019-08-05 17:39:19 -0500

    first flow for confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
+     skip_before_action :authorize_request
 
       def show
         user = User.find_by(email: "#{params["email"]}.#{params[:format]}")
@@ -17,12 +18,25 @@ module Api
       end
 
       def create
-        user = Users::Create::Execute.new.(user_params)
+        @user = Users::Create::Execute.new.(user_params)
 
-        if user.success?
-          render json: user.success, status: :ok, serializer: UsersSerializer
+        if @user.success?
+          UserMailer.with(user: @user.success).welcome_email.deliver_now
+          render json: @user.success, status: :ok, serializer: UsersSerializer
         else
-          render json: { errors: user.failure[:message] }, status: :not_found
+          render json: { errors: @user.failure[:message] }, status: :not_found
+        end
+      end
+
+      def confirm_email
+        p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        p user = User.find_by(verification_code: params[:id])
+        p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        if user
+          user.email_activate
+          p "SUCCESS ACTIVACTION"
+        else
+          p "error in the verificatoin"
         end
       end
 
