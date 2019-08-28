785a5b646cb32acb03c9b35ed976bae1794a72ef
wecalderonc <wecalderonc@unal.edu.co>
2019-08-28 15:32:28 -0500

    added specs to integration accumulator report

--- spec/integration/accumulators_report_spec.rb
+++ spec/integration/accumulators_report_spec.rb
@@ -3,7 +3,7 @@ require 'swagger_helper'
 RSpec.describe "Accumulators API", :type => :request do
 
   path "/api/v1/accumulators_report/" do
-    get 'Retrieves a last accumulators of a thing' do
+    get 'Retrieves last accumulators of a thing' do
       tags 'Things'
       produces 'application/json'
       parameter name: :thing_name, :in => :query, :type => :string
@@ -11,10 +11,10 @@ RSpec.describe "Accumulators API", :type => :request do
       parameter name: 'Authorization', :in => :header, :type => :string
 
       response '200', 'last accumulators for thing found' do
-        let(:user) { create(:user) }
-        let(:accumulator) { create(:accumulator) }
-        let(:thing_name) { accumulator.uplink.thing.name }
-        let(:query) { 'last_accumulators' }
+        let(:user)            { create(:user) }
+        let(:accumulator)     { create(:accumulator) }
+        let(:thing_name)      { accumulator.uplink.thing.name }
+        let(:query)           { 'last_accumulators' }
 
         let(:'Authorization') { JsonWebToken.encode({ user_id: user.id }) }
 
@@ -24,6 +24,21 @@ RSpec.describe "Accumulators API", :type => :request do
           properties: accumulator_properties,
           required: accumulator_fields
         }
+        run_test!
+      end
+
+      response '404', 'thing not found' do
+        let(:user)            { create(:user) }
+        let(:accumulator)     { create(:accumulator) }
+        let(:thing_name)      { "wrong_name" }
+        let(:query)           { 'last_accumulators' }
+
+        let(:'Authorization') { JsonWebToken.encode({ user_id: user.id }) }
+
+        schema type: :object,
+          properties: { errors: { type: :string} },
+          required: [ 'errors' ]
+
         run_test!
       end
     end
