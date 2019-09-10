b754fe4db5287427d17ca84c94a541ec04ccb0f6
Jeisson Moreno <jmoreno@procibernetica.com>
2019-09-10 16:58:22 -0500

    add alarms type hardware taken of the project save_uplink_to_neo4jdb

--- app/models/alarm_type.rb
+++ app/models/alarm_type.rb
@@ -10,4 +10,10 @@ class AlarmType
   validates :type, presence: true
 
   has_one :in, :alarm, type: :TYPE
+
+  HARDWARE_ALARMS = {
+    1 => :power_connection,
+    2 => :induced_site_alarm,
+    3 => :sos
+  }
 end
