05f0d75f53e92343800691695ad69fbb83affd88
morenojey-90 <jmoreno@procibernetica.com>
2019-10-15 10:12:34 -0500

    fixing tests

--- spec/services/reports/projected_consumption_spec.rb
+++ spec/services/reports/projected_consumption_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Reports::AverageConsumption do
+RSpec.describe Reports::ProjectedConsumption do
   describe '#call' do
     let(:response) { subject.(input) }
     let(:location) { create(:location, schedule_billing: billing) }
@@ -9,7 +9,7 @@ RSpec.describe Reports::AverageConsumption do
     let(:uplink2)  { create(:uplink, thing: thing, time: (DateTime.now - 2.months).to_i) }
     let(:uplink3)  { create(:uplink, thing: thing, time: (DateTime.now - 3.months).to_i) }
     let(:uplink4)  { create(:uplink, thing: thing, time: (DateTime.now - 2.days).to_i) }
-    let(:input)    { { thing: thing } }
+    let(:input)    { { consumptions_by_month: { '4': value: 256, days_count: 20, months: [5, 6]} } }
 
     context "consumption days higher than billing period days" do
       let(:billing)  { create(:schedule_billing, billing_frequency: 1, start_date: DateTime.now - (4.months - 8.days)) }
