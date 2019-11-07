7a386a8a96ee2825432e53f116691b13eb67976a
William Calderon <wecalderonc@unal.edu.co>
2019-11-07 16:53:08 -0500

    refactor customized

--- app/permissions/customized.rb
+++ app/permissions/customized.rb
@@ -2,12 +2,21 @@ module Permissions::Customized
   def checking_thing
     @thing = Thing.find_by(name: params.permit(:thing_name)[:thing_name])
 
-    if @thing.present?
-      if !(can? :read, @thing)
-        render json: { errors: "Access denied. You didn't own the thing #{@thing.name}"}, status: :unauthorized
-      end
-    else
-      render json: { errors: "Access denied. You didn't own the thing or the device doesnt exist"}, status: :not_found
+    options = {
+      true => hola(@thing),
+      false => render
+    }
+
+    options[@thing.present?]
+  end
+
+  def hola(thing)
+    if not (can? :read, thing)
+      render json: { errors: "Access denied. You didn't own the thing #{thing.name}"}, status: :unauthorized
     end
+  end
+
+  def render
+    render json: { errors: "Access denied. You didn't own the thing or the device doesnt exist"}, status: :not_found
   end
 end
