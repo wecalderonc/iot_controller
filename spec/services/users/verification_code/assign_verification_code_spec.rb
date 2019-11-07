3e956df43a935e6c7c386b3ea76fc8ffb7623158
morenojey-90 <jmoreno@procibernetica.com>
2019-11-07 16:25:47 -0500

    add verification code service and their respective tests

---
+++ spec/services/users/verification_code/assign_verification_code_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+Rspec.describe Users::VerificationCode::AssignVerificationCode do
+  describe '#call' do
+    let(:response) { subject.(input) }
+    let(:user) { create(:user) }
+    let(:input) { 
+      { 
+        user: user
+      } 
+    }
+
+    context ' verification code' do
+      it 'it return a success response' do
+
+        expect(response).to be(user)
+      end
+    end
+
+    context ' verification code is different' do
+      it 'it return a success response' do
+        user.verification_code = '1a2b3c4d5e6f'
+
+        expect(response).to be(user)
+        user.reload
+        expect(user.verification_code).not_to be('1a2b3c4d5e6f')
+      end
+    end
+  end
+end
---
+++ spec/services/users/verification_code/execute_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Users::VerificationCode::Execute do
+  describe "#call" do
+    let(:user)    { create(:user) }
+    let(:country) { create(:country, code_iso: 'CO') }
+
+    context "When the user obtain a new verification code" do
+
+      let(:input) {
+        {
+          email: user.email
+        }
+      }
+
+      context "When all the operations are successful" do
+        it "Should return a Success response" do
+          expect_any_instance_of(UserMailer).to receive(:update_confirmation).once
+          user.verification_code = nil
+
+          response = subject.(input)
+          user.reload
+          expect(response).to be_success
+          expect(response.success).to eq(user)
+          expect(user.verification_code).not_to be(nil)
+        end
+      end
+
+      context "When the email does not match the one the user created" do
+        it "Should return a Success response" do
+          input[:email] = "jconnor@bad"
+
+          response = subject.(input)
+          expected_response = "User not found"
+          expect(response).to be_failure
+          expect(response.failure[:message]).to eq(expected_response)
+        end
+      end
+    end
+  end
+end
