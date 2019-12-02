63e8877b5712ef89cf4ef642fa3a738f1f8b99c1
William Calderon <wecalderonc@unal.edu.co>
2019-12-02 17:29:35 -0500

    added last_digit to utils

--- app/serializers/battery_level_serializer.rb
+++ app/serializers/battery_level_serializer.rb
@@ -2,7 +2,7 @@ class BatteryLevelSerializer < UplinkBaseSerializer
   attributes :level_label
 
   def level_label
-    battery_value = object.value[-1].to_i
+    battery_value = Utils.last_digit(object.value).to_i
     BatteryLevel::LEVEL_LABELS[battery_value]
   end
 end
