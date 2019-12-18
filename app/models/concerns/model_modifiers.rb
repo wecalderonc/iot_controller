3cb2c82f67765ba47c468142916c256ceabf34fc
William Calderon <wecalderonc@unal.edu.co>
2019-12-18 16:18:51 -0500

    add spec for concern and modified names

--- app/models/concerns/model_modifiers.rb
+++ app/models/concerns/model_modifiers.rb
@@ -1,9 +1,9 @@
 module ModelModifiers
   extend ActiveSupport::Concern
 
-  def delete_objects_with_property!(objects, property)
-    objects.map{|_, x|
-      x.reject! { |y| y.send(property) }
+  def delete_objects_with_property!(thing_with_objects, property)
+    thing_with_objects.map{|_, objets_array|
+      objets_array.reject! { |object| object.send(property) }
     }
   end
 end
